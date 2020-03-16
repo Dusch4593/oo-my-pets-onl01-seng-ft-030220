03c4102f3e23f06be6797684c56c52d13ebcae48
Brandon Dusch <brandondusch@gmail.com>
2020-03-16 02:43:53 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,50 @@
+require 'pry'
+
 class Owner
   # code goes here
+  @@all = []
+  attr_reader :name, :species 
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def species 
+    @species = "human"
+  end
+  
+  def say_species 
+    "I am a #{species}."
+  end
+  
+  def self.all 
+    @@all
+  end
+  
+  def self.count 
+    all.count
+  end
+  
+  def self.reset_all 
+    all.clear
+  end
+  
+  def cats 
+    Cat.all.select { |cat| cat.owner == self }
+  end
+  
+  def dogs 
+    Dog.all.select { |dog| dog.owner == self }
+  end
+  
+  def buy_cat(name)
+    new_cat = Cat.new(name, self)
+    cats << new_cat
+  end
+  
+  def buy_dog(name)
+    new_dog = Dog.new(name, self)
+    dogs << new_dog
+  end
 end
